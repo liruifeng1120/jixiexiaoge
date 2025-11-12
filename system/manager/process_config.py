e4d51b67527fa350980a8bad8502dc73e3b5c62a
Rick Lan <ricklan@gmail.com>
2025-11-12 13:04:27 +0800

    Fix: disable connect not working properly

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -64,8 +64,8 @@ def dashy(started: bool, params: Params, CP: car.CarParams) -> bool:
 def dashy_with_video(started: bool, params: Params, CP: car.CarParams) -> bool:
   return int(params.get("dp_dev_dashy") or 0) == 2
 
-def disable_connect(started: bool, params: Params, CP: car.CarParams) -> bool:
-  return params.get_bool("dp_dev_disable_connect")
+def connect(started: bool, params: Params, CP: car.CarParams) -> bool:
+  return not params.get_bool("dp_dev_disable_connect")
 
 def or_(*fns):
   return lambda *args: operator.or_(*(fn(*args) for fn in fns))
@@ -74,7 +74,7 @@ def and_(*fns):
   return lambda *args: operator.and_(*(fn(*args) for fn in fns))
 
 procs = [
-  DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid", enabled=not disable_connect),
+  DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid"),
 
   NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
   NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
@@ -118,7 +118,7 @@ procs = [
   PythonProcess("hardwared", "system.hardware.hardwared", always_run),
   PythonProcess("tombstoned", "system.tombstoned", always_run, enabled=not PC),
   PythonProcess("updated", "system.updated.updated", only_offroad, enabled=not PC),
-  PythonProcess("uploader", "system.loggerd.uploader", always_run, enabled=not disable_connect),
+  PythonProcess("uploader", "system.loggerd.uploader", connect and always_run),
   PythonProcess("statsd", "system.statsd", always_run),
   PythonProcess("feedbackd", "selfdrive.ui.feedback.feedbackd", only_onroad, enabled="LITE" not in os.environ),
 
